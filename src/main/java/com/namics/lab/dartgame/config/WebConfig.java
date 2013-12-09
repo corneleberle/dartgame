4ce36c39ead2db503088ae0c14d715d3a457c14a
Cornel Eberle <cornel.eberle@namics.com>
2013-12-09 14:00:21 +0100

    add sample code

---
+++ src/main/java/com/namics/lab/dartgame/config/WebConfig.java
@@ -0,0 +1,53 @@
+package com.namics.lab.dartgame.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.namics.lab.dartgame.echo.DefaultEchoService;
+import com.namics.lab.dartgame.echo.EchoWebSocketHandler;
+
+@Configuration
+@EnableWebMvc
+@EnableWebSocket
+public class WebConfig extends WebMvcConfigurerAdapter implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+		registry.addHandler(echoWebSocketHandler(), "/echo");
+		// registry.addHandler(snakeWebSocketHandler(), "/snake");
+
+		registry.addHandler(echoWebSocketHandler(), "/sockjs/echo").withSockJS();
+		// registry.addHandler(snakeWebSocketHandler(), "/sockjs/snake").withSockJS();
+	}
+
+	@Bean
+	public WebSocketHandler echoWebSocketHandler() {
+		return new EchoWebSocketHandler(echoService());
+	}
+
+	// @Bean
+	// public WebSocketHandler snakeWebSocketHandler() {
+	// return new PerConnectionWebSocketHandler(SnakeWebSocketHandler.class);
+	// }
+
+	@Bean
+	public DefaultEchoService echoService() {
+		return new DefaultEchoService("Did you say \"%s\"?");
+	}
+
+	// Allow serving HTML files through the default Servlet
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+}
