9eec4bdbe0a05d7ce66203decfd054523ca3a5df
Cornel Eberle <cornel.eberle@namics.com>
2013-12-12 15:57:23 +0100

    add message property

--- src/main/java/com/namics/lab/dartgame/message/StatusMessage.java
+++ src/main/java/com/namics/lab/dartgame/message/StatusMessage.java
@@ -2,4 +2,14 @@ package com.namics.lab.dartgame.message;
 
 public class StatusMessage extends AbstractMessage {
 
+	private StatusType statusType;
+
+	public StatusType getStatusType() {
+		return statusType;
+	}
+
+	public void setStatusType(StatusType statusType) {
+		this.statusType = statusType;
+	}
+
 }
