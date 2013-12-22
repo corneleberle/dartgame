fcdae3d899479a6ddd73b0d65f9ea003ba993bca
Adrian Scherrer <adrian.scherrer@namics.com>
2013-12-22 02:20:29 +0100

    improve canvas drawing

--- src/main/dart/web/dartgame.dart
+++ src/main/dart/web/dartgame.dart
@@ -8,8 +8,7 @@ import 'messages.dart';
 const String SHOT_COLOR = "orange";
 const String GROUND_COLOR = "grey";
 const String CANNON_COLOR = "red";
-const int SEED_RADIUS = 1;
-const num TAU = PI * 2;
+const String HIT_COLOR = "yellow";
 
 const num WIND_FACTOR = 0.1;
 const num GRAVITY_FACTOR = 0.5;
@@ -20,15 +19,23 @@ final InputElement triggerButton = querySelector("#trigger");
 final InputElement connectButton = querySelector("#connect");
 final ImageElement windsockImage = querySelector("#windsock");
 
-final CanvasRenderingContext2D context =
-  (querySelector("#canvas") as CanvasElement).context2D;
-final int CANVAS_WIDTH = context.canvas.width;
-final int CANVAS_HEIGHT = context.canvas.height;
+//final CanvasRenderingContext2D context = (querySelector("#canvas") as CanvasElement).context2D;
+final CanvasElement masterCanvas = querySelector("#canvas") as CanvasElement;
+final int CANVAS_WIDTH = masterCanvas.width;
+final int CANVAS_HEIGHT = masterCanvas.height;
 
 final int SCALE_X = CANVAS_WIDTH;
 final int SCALE_Y = CANVAS_HEIGHT;
 final int OFFSET_X = 0;
 final int OFFSET_Y = CANVAS_HEIGHT;
+
+// Different layers to draw on.
+// For performance reasons the landscape shall only be drawn once, whereas cannon, fligth path, etc. are redrawn very often
+final CanvasElement cannonLeftCanvas = new CanvasElement(width: CANVAS_WIDTH, height: CANVAS_HEIGHT);
+final CanvasElement cannonRightCanvas = new CanvasElement(width: CANVAS_WIDTH, height: CANVAS_HEIGHT);
+final CanvasElement flightpathCanvas = new CanvasElement(width: CANVAS_WIDTH, height: CANVAS_HEIGHT);
+final CanvasElement landscapeCanvas = new CanvasElement(width: CANVAS_WIDTH, height: CANVAS_HEIGHT);
+final CanvasElement windsockCanvas = new CanvasElement(width: CANVAS_WIDTH, height: CANVAS_HEIGHT);
 
 WebSocket webSocket = null;
 List<num> landscape = new List(1000);
@@ -59,7 +66,7 @@ void fillLandscapeWithTestData(List<num> landscape) {
 
 
 void updateCannon(Cannon cannon) {
-  cannon.angle = int.parse(angleSlider.value) / 1000 * PI; 
+  cannon.angle = PI - int.parse(angleSlider.value) / 1000 * PI; 
   cannon.power = int.parse(powerSlider.value) / 1000;
   drawCannon(cannon);
 }
@@ -68,34 +75,59 @@ void updateCannon(Cannon cannon) {
 void drawCannon(Cannon cannon) {
   double deltaX = 0.1 * (1 + cannon.power) * cos(cannon.angle);
   double deltaY = 0.1 * (1 + cannon.power) * sin(cannon.angle);
-  context..beginPath()
-         ..lineWidth = 2
-         ..fillStyle = CANNON_COLOR
-         ..strokeStyle = CANNON_COLOR
-         ..moveTo(scaleX(cannon.pos.x), scaleY(cannon.pos.y))
-         ..lineTo(scaleX(cannon.pos.x + deltaX), scaleY(cannon.pos.y + deltaY))
-         ..closePath()
-         ..stroke();
+
+  CanvasElement tempCanvas;
+  if (cannon == leftCannon) {
+    tempCanvas = cannonLeftCanvas;
+  } else {
+    tempCanvas = cannonRightCanvas;
+  }
+  tempCanvas.context2D
+      ..clearRect(0, 0, tempCanvas.width, tempCanvas.height)
+      ..beginPath()
+      ..lineWidth = 2
+      ..fillStyle = CANNON_COLOR
+      ..strokeStyle = CANNON_COLOR
+      ..moveTo(scaleX(cannon.pos.x), scaleY(cannon.pos.y))
+      ..lineTo(scaleX(cannon.pos.x + deltaX), scaleY(cannon.pos.y + deltaY))
+      ..closePath()
+      ..stroke();
+
+  redrawCanvas();
 }
 
 void drawShotCurve(Cannon cannon) {
   num time = 0;
   num x = 0;
   num y = 0;
-  bool flying=true;
+  bool flying = true;
   
-  while (flying) {
+  flightpathCanvas.context2D
+      ..clearRect(0, 0, flightpathCanvas.width, flightpathCanvas.height);
+  
+  num i = 0;
+  do {
+    time = i * 0.01;
+    
     // Normalized position
     x = cannon.pos.x + (2 * cannon.power * time * cos(cannon.angle) + WIND_FACTOR * wind * time);
     y = cannon.pos.y + (2 * cannon.power * time * sin(cannon.angle) - GRAVITY_FACTOR * time * time);
     
-    drawLine(scaleX(x), scaleY(y), SHOT_COLOR);
-
-    if (x<=0 || x>=1 || y<0 || hitGround(x,y)) {
+    if (x<=0 || x>=1 || y<0) {
       flying = false;
+    } else if (hitGround(x,y)) {
+      flying = false;
+      drawCircle(flightpathCanvas, scaleX(x), scaleY(y), HIT_COLOR, 4);
+      redrawCanvas();
     }
-    time += 0.001;
-  }
+    
+    if (i % 5 == 0) {
+      drawCircle(flightpathCanvas, scaleX(x), scaleY(y), SHOT_COLOR, 1);
+      redrawCanvas();
+    }
+    
+    i += 1;
+  } while (flying);
 }
 
 void sendShotRequest(Cannon cannon) {
@@ -105,44 +137,59 @@ void sendShotRequest(Cannon cannon) {
 }
 
 /// Draw a small circle representing the bomb.
-void drawLine(num x, num y, String color) {
-  context..beginPath()
-         ..lineWidth = 2
-         ..fillStyle = color
-         ..strokeStyle = color
-         ..arc(x, y, SEED_RADIUS, 0, TAU, false)
-         ..fill()
-         ..closePath()
-         ..stroke();
+void drawCircle(CanvasElement canvas, num x, num y, String color, num radius) {
+  canvas.context2D
+      ..beginPath()
+      ..lineWidth = 2
+      ..fillStyle = color
+      ..strokeStyle = color
+      ..arc(x, y, radius, 0, 2*PI, false)
+      ..fill()
+      ..closePath()
+      ..stroke();
 }
 
-void drawMapProfile(List<num> mapProfile) {
-  context..beginPath()
-    ..lineWidth = 2
-    ..fillStyle = GROUND_COLOR
-    ..strokeStyle = GROUND_COLOR
-    ..moveTo(0, scaleY(mapProfile.elementAt(0)));
+void drawLandscape(List<num> mapProfile) {
+  landscapeCanvas.context2D
+      ..beginPath()
+      ..lineWidth = 2
+      ..fillStyle = GROUND_COLOR
+      ..strokeStyle = GROUND_COLOR
+      ..moveTo(0, scaleY(mapProfile.elementAt(0)));
   
   for(int i = 0; i < mapProfile.length; i++){
-    context.lineTo(scaleX(1/mapProfile.length * i), scaleY(mapProfile.elementAt(i)));
+    landscapeCanvas.context2D.lineTo(scaleX(1/mapProfile.length * i), scaleY(mapProfile.elementAt(i)));
   }
   
   //finish
-  context..lineTo(scaleX(1.0), scaleY(mapProfile.last))
-         ..lineTo(scaleX(1.0), scaleY(0.0))
-         ..lineTo(scaleX(0.0), scaleY(0.0))
-         ..fill()
-         ..closePath()
-         ..stroke();
+  landscapeCanvas.context2D
+      ..lineTo(scaleX(1.0), scaleY(mapProfile.last))
+      ..lineTo(scaleX(1.0), scaleY(0.0))
+      ..lineTo(scaleX(0.0), scaleY(0.0))
+      ..fill()
+      ..closePath()
+      ..stroke();
 }
 
 void drawWindsock(num force) {
   final CanvasElement tempCanvas = new CanvasElement(width: 200, height: 200);
-  CanvasRenderingContext2D tempContext = tempCanvas.context2D;
-  tempContext..translate(100, 100)
-             ..rotate(-force * PI / 2)
-             ..drawImage(windsockImage, -windsockImage.width/2, 0);
-  context..drawImage(tempCanvas, 0, -90);
+  tempCanvas.context2D
+      ..translate(100, 100)
+      ..rotate(-force * PI / 2)
+      ..drawImage(windsockImage, -windsockImage.width/2, 0);
+
+  windsockCanvas.context2D
+      ..drawImage(tempCanvas, 0, -90);
+}
+
+void redrawCanvas() {
+  masterCanvas.context2D
+      ..clearRect(0, 0, masterCanvas.width, masterCanvas.height)
+      ..drawImage(landscapeCanvas, 0, 0)
+      ..drawImage(cannonLeftCanvas, 0, 0)
+        ..drawImage(cannonRightCanvas, 0, 0)
+      ..drawImage(windsockCanvas, 0, 0)
+      ..drawImage(flightpathCanvas, 0, 0);
 }
 
 bool hitGround(num x, num y) {
@@ -204,10 +251,10 @@ void connect(MouseEvent event) {
           rightCannon = myCannon;
         }
         
-        context.clearRect(0, 0, SCALE_X, SCALE_Y);
-        drawMapProfile(landscape);
+        drawLandscape(landscape);
         drawWindsock(wind);
         updateCannon(myCannon);
+        updateCannon(enemyCannon);
       }
       
       if(message["messageType"] == MessageTypesEnum.MESSAGE_TYPE_SHOT){
@@ -215,8 +262,14 @@ void connect(MouseEvent event) {
         double power = message["power"];
         
         if (message["shooter"] == "LEFT") {
+          leftCannon.angle = angle;
+          leftCannon.power = power;
+          drawCannon(leftCannon);
           drawShotCurve(leftCannon);
         } else {
+          rightCannon.angle = angle;
+          rightCannon.power = power;
+          drawCannon(rightCannon);
           drawShotCurve(rightCannon);
         }
       }
